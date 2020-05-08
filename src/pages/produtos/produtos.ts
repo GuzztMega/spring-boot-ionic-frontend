0b8a24f62cff751c136f134b01657aebdf7b0c8d
GuzzMega <guzzt.mega@gmail.com>
2020-05-08 11:07:39 -0300

    Infinity Scroll

--- src/pages/produtos/produtos.ts
+++ src/pages/produtos/produtos.ts
@@ -12,7 +12,8 @@ import { API_CONFIG } from '../../config/api.config';
 })
 export class ProdutosPage {
 
-  items: ProdutoDTO[];
+  items: ProdutoDTO[] = [];
+  page: number = 0;
 
   constructor(
     public navCtrl: NavController, 
@@ -27,18 +28,22 @@ export class ProdutosPage {
   loadData(){
     let categoria_id = this.navParams.get("categoria_id");
     let loader = this.presentLoading();
-    this.produtoService.findByCategoria(categoria_id)
+    this.produtoService.findByCategoria(categoria_id, this.page, 10)
       .subscribe( response => {
-        this.items = response["content"];
+        let start = this.items.length;
+        this.items = this.items.concat(response["content"]);
+        let end = this.items.length-1;
         loader.dismiss();
-        this.loadImageUrls();
+        console.log(this.page);
+        console.log(this.items);
+        this.loadImageUrls(start, end);
       }, error => {
         loader.dismiss();
       });
   }
 
-  loadImageUrls(){
-    for(var i=0; i < this.items.length; i++){
+  loadImageUrls(start, end){
+    for(var i=start; i < end; i++){
       let item = this.items[i];
       this.produtoService.getThumbnailFromBucket(item.id)
       .subscribe( response => {
@@ -62,8 +67,18 @@ export class ProdutosPage {
   doRefresh(refresher){
 
     setTimeout(() => {
+      this.page = 0;
+      this.items = [];
       this.loadData();
       refresher.complete();
+    }, 1000);
+  }
+
+  doInfinite(infiniteScroll){
+    this.page++;
+    this.loadData();
+    setTimeout(() => {
+      infiniteScroll.complete();
     }, 1000);
   }
 }
