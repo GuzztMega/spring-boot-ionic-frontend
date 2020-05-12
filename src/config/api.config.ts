56d0ec24bf18d3fd5c9e7f18abe68b2cbee26db0
GuzzMega <guzzt.mega@gmail.com>
2020-05-12 12:23:53 -0300

    "Testando o app no dispositivo

--- src/config/api.config.ts
+++ src/config/api.config.ts
@@ -1,4 +1,4 @@
 export const API_CONFIG = {
-    baseUrl: "http://localhost:8080",
+    baseUrl: "https://udemyspring.herokuapp.com",
     bucketBaseUrl: "https://udemy-springionic.s3-sa-east-1.amazonaws.com/"
 }
