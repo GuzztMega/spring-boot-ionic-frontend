053e5ea8ed74a7c9bec855ca28f4bb7aa1ee5b16
GuzzMega <guzzt.mega@gmail.com>
2020-05-18 12:35:36 -0300

    Finalizando aplicação

--- src/pages/profile/profile.ts
+++ src/pages/profile/profile.ts
@@ -64,10 +64,11 @@ export class ProfilePage {
     });
   }
 
-  blobToDataUrl(blob){
-    return new Promise((fulfill, reject) => {
-      let reader = new FileReader();
-      reader.onerror = reject;
+  // https://gist.github.com/frumbert/3bf7a68ffa2ba59061bdcfc016add9ee
+  blobToDataUrl(blob) {
+    return new Promise((fulfill, reject) => {        
+      let reader = new FileReader();        
+      reader.onerror = reject;      
       reader.onload = (e) => fulfill(reader.result);
       reader.readAsDataURL(blob);
     })
@@ -78,10 +79,11 @@ export class ProfilePage {
     this.cameraOn = true;
     
     const options: CameraOptions = {
-      quality: 100,
+      quality: 75,
       destinationType: this.camera.DestinationType.DATA_URL,
-      encodingType: this.camera.EncodingType.PNG,
-      mediaType: this.camera.MediaType.PICTURE
+      encodingType: this.camera.EncodingType.JPEG,
+      mediaType: this.camera.MediaType.PICTURE,
+      correctOrientation: true
     }
 
     this.camera.getPicture(options).then((imageData) => {
